aba277f7cf02915187e22ab7a58ff64248c3ac60
Tu Chu <tuchuthien2710@gmail.com>
2026-09-23 20:39:48 +0700

    update buoi 6

--- obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
+++ obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3395513a8538bdec947040a9ba080d6f8a8aeef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5506e8b1042e94cc91f85405a4c06ef53b2c4acd")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
